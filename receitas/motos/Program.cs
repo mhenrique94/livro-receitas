959ac230e6ab347912e4253f13cee32c5842acc8
mhenrique94 <mhenrique94@outlook.com>
2020-12-20 19:31:48 -0300

    Incluido desafio 2 - motos

---
+++ receitas/motos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace motos
+{
+    class Program
+    {
+        static void Main() {
+            Console.WriteLine("Digite quantos kilometros de distância entre moto X e Y");
+            int quilometros = Int32.Parse(Console.ReadLine());
+            int minutos = quilometros * 2; // Digite aqui o calculo dos minutos
+            Console.WriteLine(minutos + " minutos");
+        }
+    }
+}
