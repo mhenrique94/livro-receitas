6f457c06b63aa8f7fd065c35299f0cd21f73c3e7
mhenrique94 <mhenrique94@outlook.com>
2020-12-20 22:50:28 -0300

    adding ASNPNET structure

---
+++ receitas/desafio3_mandioca/aipim/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace aipim
+{
+    public struct Program
+    {
+        static void Main() {
+
+            try
+            {
+
+            int chico = 300 * Int32.Parse(Console.ReadLine());
+            int bento = 1500 * Int32.Parse(Console.ReadLine());
+            int bernardo = 600 * Int32.Parse(Console.ReadLine());
+            int marina = 1000 * Int32.Parse(Console.ReadLine());
+            int iara = 150 * Int32.Parse(Console.ReadLine());
+            int marlene = 225;
+            int total = chico + bento + bernardo + marina + iara + marlene; // Digite aqui o calculo total
+            Console.WriteLine(total);
+            }
+                catch(FormatException)
+                    {
+                                Console.WriteLine("Erro, tente novamente digitando um número inteiro e pressionando ENTER!");
+                    }          
+        }
+    }
+}
