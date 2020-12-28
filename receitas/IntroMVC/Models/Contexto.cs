78001d608623f00f39318cad2e15af956e527b1e
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2020-12-27 22:08:17 -0300

    IntroMVC
    Criado novo projeto de introdução ao desenvolvimento ASPNET com estrutura MVC

---
+++ receitas/IntroMVC/Models/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroMVC.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=introMVC;Integrated Security=true");
+
+        }
+    }
+}
--- receitas/asnpnet_intro/Models/Categoria.cs
+++
@@ -1,9 +0,0 @@
-namespace asnpnet_intro.Models
-{
-    public class Categoria
-    {
-        public int id {get;set;}
-        public string descricao {get; set;}
-
-    }
-}
