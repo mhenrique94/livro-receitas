3275eba6f1997f72ffac7ef2ebde867f4db1cd3d
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2020-12-29 14:24:33 -0300

    Added desafios

---
+++ receitas/ajuda_Nath/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class conversaoDeTempo {
+
+static void Main() {
+
+    var tempo = Int32.Parse(Console.ReadLine());
+
+    if(tempo > 0 ){
+
+    int horas = tempo / 3600;
+
+    int minutos = (tempo % 3600) / 60 ;
+
+    int segundos =  (tempo % 3600) % 60;
+
+    Console.WriteLine($"" + horas + ":" + minutos + ":" + segundos);
+    } else{
+        Console.WriteLine("Insira um numero válido!");
+    }
+}
+}
---
+++ receitas/desafio1_essenciais/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+    class Desafio
+    {
+        static void Main(){
+
+        try{
+                
+            Console.WriteLine("\nInsira um número de 1 a 1000: ");
+            var meuNumero = Int32.Parse(Console.ReadLine());
+
+                for (int i=1; i<= meuNumero; i++)
+                {
+                    var resultado = $"{i} {i*i} {i*i*i}";
+                    //var resultado = $"{i} {i^2} {i^3}";
+                    Console.WriteLine(resultado);
+                }
+
+        }
+                
+                catch(FormatException){
+                    Console.WriteLine("\nInsira um número de 1 a 1000! ");
+                }
+                finally{
+                    var meuNumero = Int32.Parse(Console.ReadLine());
+
+                for (int i=1; i<= meuNumero; i++)
+                {
+                    var resultado = $"{i} {i*i} {i*i*i}";
+                    Console.WriteLine(resultado);
+                }
+                }
+        }
+        }
+
+//PARA O CONSOLE DA DIO
+// using System;
+
+// class Desafio {
+//     static void Main() {
+//         var numero = Int32.Parse(Console.ReadLine());
+        
+//         if(numero != 0)
+//         {
+//           for(int i = 1; i <= numero; i++)
+//           {
+//             var numeros = $"{i} {i * i} {i * i * i}";
+//             Console.WriteLine(numeros);
+//           }
+//         } else {
+//           Console.WriteLine("Insira um número!");
+//         }
+//     }
+// }
