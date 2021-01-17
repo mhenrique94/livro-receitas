88bf3f1a44366d5d9ddcf45d9760bd83ab231669
mhenrique94 <mhenrique94@outlook.com>
2021-01-17 01:11:49 -0300

    Adição de alguns scripts e botões

--- receitas/introHTML/main.js
+++ receitas/introHTML/main.js
@@ -1,2 +1,18 @@
 // alert("meu primeiro js");
 
+
+function clicou(){
+    var disp = document.getElementById("post")
+
+    if(disp.style.display === "none"){
+        disp.style.display = 'block';
+    }
+    
+    else{
+        disp.style.display = 'none'
+    }
+}
+
+function redirecionar(){
+    window.open("https://github.com/mhenrique94");
+}
